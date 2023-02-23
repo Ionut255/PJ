7cf2ae6d46324baa7cc11c3c8bb6e3228ec7c295
Ionut Sprincenatu <112524220+Ionut255@users.noreply.github.com>
2023-02-23 16:42:47 +0200

    ..

--- Pj.java
+++ Pj.java
@@ -1,15 +1,24 @@
-//package capitolul1.hello;
-import java.util.Scanner;
-import java.util.*;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.util.Scanner;
+import java.io.IOException;
 public class Pj {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)throws IOException {
+        /*
         System.out.println("a=");
         Scanner scanner = new Scanner(System.in);
         int a=scanner.nextInt();
         System.out.println("Ati introdus valoarea = "+a);
         scanner.close();
+        */
+        BufferedReader flux_in = new BufferedReader(new InputStreamReader (System.in));
+        System.out.println("Introduceti linia: ");
+        String s = flux_in.readLine();
+        System.out.println("Ati introdus linia: "+s);
+        flux_in.close();
     }
 
 }
---
+++ Pj_lab1.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+
+public class Pj_lab1 {
+
+    public static void main(String[] args) {
+        System.out.println("L=");
+        Scanner scanner = new Scanner(System.in);
+        int L=scanner.nextInt();
+        System.out.println("l=");
+        Scanner scanner1 = new Scanner(System.in);
+        int l=scanner1.nextInt();
+       
+        int A=L*l;
+        int P=2*(L+l);
+        System.out.println("Aria este = "+A);
+        System.out.println("Perimetru este = "+P);
+        scanner.close();
+        scanner1.close();
+    }
+
+
+
+
+    
+
+}
