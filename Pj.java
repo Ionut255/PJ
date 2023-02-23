e8373e65396734d371d569637dfcfc5991371f51
Ionut Sprincenatu <112524220+Ionut255@users.noreply.github.com>
2023-02-23 16:05:28 +0200

    Create Pj.java

---
+++ Pj.java
@@ -0,0 +1,15 @@
+//package capitolul1.hello;
+import java.util.Scanner;
+import java.util.*;
+
+public class Pj {
+
+    public static void main(String[] args) {
+        System.out.println("a=");
+        Scanner scanner = new Scanner(System.in);
+        int a=scanner.nextInt();
+        System.out.println("Ati introdus valoarea = "+a);
+        scanner.close();
+    }
+
+}
